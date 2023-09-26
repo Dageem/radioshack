db22d8af071716652e807831a1623c434f718dc4
jacobhogsten <hogsten1996@gmail.com>
2023-09-26 14:29:43 -0400

    cartitems + imgUrls

--- src/reducers/api.js
+++ src/reducers/api.js
@@ -1,16 +1,35 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-// Define a service using a base URL and expected endpoints
 export const api = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({baseUrl: "localhost:3000"}),
-    endpoints: (builder) => ({
-        getProducts: builder.query({
-            query: () => "/api/products",
-          }),
-
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => '/api/products',
     }),
-})
+    getCartItems: builder.query({
+      query: () => '/api/cartitems',
+    }),
+    addCartItem: builder.mutation({
+      query: (newItem) => ({
+        url: '/api/cartitems',
+        method: 'POST',
+        body: newItem,
+      }),
+    }),
+    deleteCartItem: builder.mutation({
+      query: (id) => ({
+        url: `/api/cartitems/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
 
-export const {useGetProductsQuery}=api
+export const {
+  useGetProductsQuery,
+  useGetCartItemsQuery,
+  useAddCartItemMutation,
+  useDeleteCartItemMutation,
+} = api;
