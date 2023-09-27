035fec63a51e10f3cbc5e0f2a69ca451255f9a42
jacobhogsten <hogsten1996@gmail.com>
2023-09-27 12:51:13 -0400

    product-pages-layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,21 @@
-import Earbuds from "./components/earbuds/Earbuds";
-
-
+import React from 'react';
+import {  Route, Routes } from 'react-router-dom';
+import Earbuds from './components/earbuds/Earbuds';
+import Headphones from './components/headphones/Headphones';
+import Speakers from './components/speakers/Speakers';
+import SingleProduct from './components/singleproduct/SingleProduct';
 
 function App() {
-    return(
-        <>
-        <h1>Hello World</h1>
-        <Earbuds />
-        </>
-        
-    )
+  return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/earbuds" element={<Earbuds />} />
+        <Route path="/speakers" element={<Speakers />} />
+        <Route path="/headphones" element={<Headphones />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+      </Routes>
+  );
 }
 
 export default App;
+
---
+++ src/components/singleproduct/SingleProduct.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetProductsByIdQuery } from "../../reducers/api";
+import "./singleProduct.css";
+
+function SingleProduct() {
+  const { id } = useParams();
+  const { data: product, error, isLoading } = useGetProductsByIdQuery(id);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading product!</p>;
+  if (!product) return <p>Product not found!</p>;
+
+  return (
+    <div className="single-product">
+      <div className="product__image-container">
+        <img src={product.imageUrl} alt={product.name} />
+      </div>
+      <div className="product__details-container">
+        <h1>{product.name}</h1>
+        <p>
+          <strong>${parseFloat(product.price).toFixed(2)}</strong>
+        </p>
+        <p>
+          {product.details} Lorem ipsum dolor, sit amet consectetur adipisicing
+          elit. Maxime perspiciatis alias nihil expedita ratione? Dicta illum
+          autem molestiae modi in! Minus adipisci excepturi ab earum deleniti at
+          cum, tempora quas?
+        </p>
+      </div>
+      <div className="product__button-container">
+        <button className="addToCart">Add to Cart</button>
+        <button>Buy Now</button>
+      </div>
+      <div className="product__desc-container">
+        {/* <div>
+          eventual secondary image...
+        </div> */}
+        <p>
+          <strong>Our Opinion: </strong>Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Ipsam tenetur sed doloribus accusantium debitis
+          culpa quae earum quasi numquam aspernatur, repellendus sunt iste
+          dolorem similique itaque. Quas nam eveniet adipisci. Lorem ipsum dolor
+          sit, amet consectetur adipisicing elit. Corrupti, nesciunt ex odio
+          soluta illo iste. Est aut nemo similique suscipit quas facere fugiat
+          velit nisi harum dolorum? Magnam, amet similique.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SingleProduct;
