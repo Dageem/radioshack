6c64433b39db9021403eec5883aa8a553db6d583
dageem <dagtin1@gmail.com>
2023-10-02 14:40:18 -0400

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,45 @@
 import { Link } from "react-router-dom";
 import { useLogoutMutation } from "../../reducers/auth";
 import { useSelector } from "react-redux";
-import "./navbar.css"
+import "./navbar.css";
+import React, { useState } from "react";
 
 function Nav() {
   const [logout] = useLogoutMutation();
   const user = useSelector((state) => state.auth.credentials.user) || "";
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav>
-      <div className="navbar">
-        <div className="links">
-          <Link to={"/"}>Home</Link>
-          <Link to={"/headphones"}>HeadPhones</Link>
-          <Link to={"/earbuds"}>EarBuds</Link>
-          <Link to={"/speakers"}>Speaker</Link>
-          <Link to={"/cart"}>Cart</Link>
-          <Link to={"/login"}>Login</Link>
-        </div>
-        {user.userId && <Link to={"/User"}>Profile</Link>}
-        {user.userId && <button onClick={logout}>Logout</button>}
+      <Link to="/" className="title">
+        Home
+      </Link>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <Link to={"/headphones"}>HeadPhones</Link>
+        </li>
+        <li>
+          <Link to={"/earbuds"}>EarBuds</Link>
+        </li>
+        <li>
+          <Link to={"/speakers"}>Speaker</Link>
+        </li>
+        <li>
+          <Link to={"/cart"}>Cart</Link>
+        </li>
+        <li>
+          <Link to={"/login"}>Login</Link>
+        </li>
+        <li>{user.userId && <Link to={"/User"}>Profile</Link>}</li>
+      </ul>
+
+      {user.userId && <button onClick={logout}>Logout</button>}
+
       {user.userId && <h1>Welcome {user.email}</h1>}
     </nav>
   );
