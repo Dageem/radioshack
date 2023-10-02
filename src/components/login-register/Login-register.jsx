4618722741ad6d9511aa1f07976b05f4835da7dc
danate1997 <daniel.atehortua1997@gmail.com>
2023-10-02 16:41:10 -0400

    login page styled

--- src/components/login-register/Login-register.jsx
+++ src/components/login-register/Login-register.jsx
@@ -1,75 +1,85 @@
-import React, { useState } from 'react';
-import { useRegisterMutation, useLoginMutation } from '../../reducers/auth';
+import React, { useState } from "react";
+import { useRegisterMutation, useLoginMutation } from "../../reducers/auth";
+import "./login-register.css";
 
 const LoginRegister = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [successMessage, setSuccessMessage] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    
-    const [register] = useRegisterMutation();
-    const [login] = useLoginMutation();
-  
-    const handleRegister = async () => {
-      try {
-        await register({ email, password }).unwrap();
-        setSuccessMessage('Registered successfully!');
-      } catch (error) {
-        console.error('Register failed:', error);
-        setErrorMessage('Registration failed!');
-      }
-      setTimeout(() => {
-        setEmail('');
-        setPassword('');
-        setSuccessMessage('');
-        setErrorMessage('');
-      }, 2000);
-    };
-  
-    const handleLogin = async () => {
-      try {
-        await login({ email, password }).unwrap();
-        setSuccessMessage('Logged in successfully!');
-      } catch (error) {
-        console.error('Login failed:', error);
-        setErrorMessage('Login failed!'); 
-      }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-      setTimeout(() => {
-        setEmail('');
-        setPassword('');
-        setSuccessMessage('');
-        setErrorMessage('');
-      }, 2000);
-    };
-  
-    return (
-      <div>
-        <div className='inputContain'>
+  const [register] = useRegisterMutation();
+  const [login] = useLoginMutation();
+
+  const handleRegister = async () => {
+    try {
+      await register({ email, password }).unwrap();
+      setSuccessMessage("Registered successfully!");
+    } catch (error) {
+      console.error("Register failed:", error);
+      setErrorMessage("Registration failed!");
+    }
+    setTimeout(() => {
+      setEmail("");
+      setPassword("");
+      setSuccessMessage("");
+      setErrorMessage("");
+    }, 2000);
+  };
+
+  const handleLogin = async () => {
+    try {
+      await login({ email, password }).unwrap();
+      setSuccessMessage("Logged in successfully!");
+    } catch (error) {
+      console.error("Login failed:", error);
+      setErrorMessage("Login failed!");
+    }
+
+    setTimeout(() => {
+      setEmail("");
+      setPassword("");
+      setSuccessMessage("");
+      setErrorMessage("");
+    }, 2000);
+  };
+
+  return (
+    <div className="big">
+      <div className="big-container">
+        <section className="input-container">
+          <h5>E-Mail</h5>
           <input
-            className='input'
-            type='email' // Type is email
-            placeholder='Email' // Placeholder is Email
+            className="input"
+            type="email" // Type is email
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          <br />
+          <h5>Password</h5>
           <input
-            className='input'
+            className="input"
             type="password"
-            placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button className='butt' onClick={handleRegister}>Register</button>
-          <button className='butt' onClick={handleLogin}>Login</button>
-        </div>
+          <br />
+          <button className="butt" onClick={handleRegister}>
+            Register
+          </button>
+          <button className="butt" onClick={handleLogin}>
+            Login
+          </button>
+        </section>
         <div>
-          {successMessage && <div className="success-alert">{successMessage}</div>}
+          {successMessage && (
+            <div className="success-alert">{successMessage}</div>
+          )}
           {errorMessage && <div className="error-alert">{errorMessage}</div>}
         </div>
       </div>
-    );
-  };
-  
-  export default LoginRegister;
+    </div>
+  );
+};
 
+export default LoginRegister;
