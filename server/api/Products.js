ab7d8c08bc3d8b6cb3b707523e8768b35250f40d
jacobhogsten <hogsten1996@gmail.com>
2023-10-03 11:57:23 -0400

    admin panel form

--- server/api/Products.js
+++ server/api/Products.js
@@ -1,42 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {PrismaClient} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-router.get('/', async (req,res,next)=>{
-    try{
-       const allProducts = await prisma.product.findMany();
-       res.send(allProducts)
-    }catch(err){
-        next(err)
+router.get("/", async (req, res, next) => {
+  try {
+    const allProducts = await prisma.product.findMany();
+    res.send(allProducts);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get("/:id", async (req, res, next) => {
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.send(product);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get("/category/:categoryName", async (req, res, next) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        category: {
+          name: req.params.categoryName,
+        },
+      },
+    });
+    res.send(products);
+  } catch (err) {
+    next(err);
+  }
+});
+router.post("/", async (req, res, next) => {
+  try {
+    if(req.body.categoryId) {
+      req.body.categoryId = parseInt(req.body.categoryId, 10);
+      if(isNaN(req.body.categoryId)) {
+        return res.status(400).send({ error: 'categoryId must be a valid integer' });
+      }
     }
-})
-router.get('/:id', async (req,res,next)=>{
-    try{
-        const product = await prisma.product.findUnique({
-            where:{
-                id: Number(req.params.id)
-            }
-        });
-        res.send(product)
-    }catch(err){
-        next(err)
-    }
-})
-router.get('/category/:categoryName', async (req, res, next) => {  ///test
-    try {
-      const products = await prisma.product.findMany({
-        where: {
-          category: {
-            name: req.params.categoryName
-          }
-        }
-      });
-      res.send(products);
-    } catch (err) {
-      next(err);
-    }
-  });
-  
+    const product = await prisma.product.create({
+      data: req.body,
+    });
+    res.status(201).send(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const product = await prisma.product.update({
+      where: { id: Number(req.params.id) },
+      data: req.body,
+    });
+    res.send(product);
+  } catch (err) {
+    next(err);
+  }
+});
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await prisma.product.delete({
+      where: { id: Number(req.params.id) },
+    });
+    res.status(204).send("Deleted Successfully");
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
+
+
--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,8 @@ import Home from "./components/home/Home";
 import SingleProduct from "./components/singleproduct/SingleProduct";
 import LoginRegister from "./components/login-register/Login-register";
 import Cart from "./components/cart/Cart";
+import AdminPanel from "./components/adminPanel/AdminPanel";
+import AddEditProds from "./components/adminPanel/AddEditProds";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
       <Route path="/speakers" element={<Speakers />} />
       <Route path="/headphones" element={<Headphones />} />
       <Route path="/product/:id" element={<SingleProduct />} />
+      <Route path="/admin" element={<AdminPanel />} />
+      <Route path="/admin/edit" element={<AddEditProds />} />
     </Routes>
     </>
   );
