563fc02bd6dde52b4e5a2d6cbf99c49427f2cc88
danate1997 <daniel.atehortua1997@gmail.com>
2023-10-05 12:40:51 -0400

    profile reload fixed

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,17 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useGetOrdersQuery } from "../../reducers/api";
 import "./userProfile.css";
 import { useSelector } from "react-redux";
 
 function UserProfile() {
-  const { data: ordersData, isLoading, isError } = useGetOrdersQuery();
   const user = useSelector((state) => state.auth.credentials.user) || "";
+  const [userId, setUserId] = useState(user.userId);
+
+  const {
+    data: ordersData,
+    isLoading,
+    isError,
+    refetch,
+  } = useGetOrdersQuery(null, {
+    skip: !userId, // Skip the query if userId is not available
+  });
 
   useEffect(() => {
     if (isError) {
       console.error("Error loading orders:", isError);
     }
   }, [isError]);
+
+  useEffect(() => {
+    // When the user changes, update the stored user ID and trigger a refetch
+    setUserId(user.userId);
+    if (user.userId) {
+      refetch();
+    }
+  }, [user.userId, refetch]);
 
   if (isLoading) {
     return <div>Loading...</div>;
